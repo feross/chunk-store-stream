84cab4797594df11d217a60079c143f5d31b18c5
Feross Aboukhadijeh <feross@feross.org>
2020-07-16 21:33:22 -0700

    package metadata

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*! chunk-store-stream. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
 const ChunkStoreReadStream = require('./read')
 const ChunkStoreWriteStream = require('./write')
 
